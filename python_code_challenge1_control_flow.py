616d71803ef7cbfc50585ae1eb632e638d35723d
Jairo Vera <jairoveracanada.gmail.com>
2023-09-18 19:55:31 -0400

    Fix bug

--- python_code_challenge1_control_flow.py
+++ python_code_challenge1_control_flow.py
@@ -5,9 +5,9 @@ Create a method that tests whether the result of taking the power of one number 
 use a conditional statement to return True if the result is greater than 5000 or return False if it is not.  In order to accomplish this, we will need the 
 following steps:
 
-- Define the function to accept two input parameters called base and exponent
-- Calculate the result of base to the power of exponent
-- Use an if statement to test if the result is greater than 5000. If it is then return True. Otherwise, return False
+- Define the function to accept two input parameters called base and exponent.
+- Calculate the result of base to the power of exponent.
+- Use an if statement to test if the result is greater than 5000. If it is then return True. Otherwise, return False.
 """
 
 def large_power(base, exponent):
